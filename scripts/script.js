e689f6a2b1d05f0f7b2aef96c7936057fff5ef27
Kamen Dimitrov <kamend@gmail.com>
2019-10-27 12:46:04 +0200

    clean up

--- scripts/script.js
+++ scripts/script.js
@@ -13,28 +13,9 @@ const canv = Scene.root.find("canvas0");
 var focalPlane = cam.focalPlane;
 const camCoords = face.cameraTransform.applyTo(face.nose.tip);
 
-// set Canvas to World, place rectangle inside
-//canv.transform.x = R.neg(camCoords.x)
-//canv.transform.y = camCoords.y;
-//canv.transform.z = R.neg(camCoords.z);
 
 var u = R.add(R.div(R.div(camCoords.x, R.div(focalPlane.width,2)),2), 0.5);
 var v = R.add(R.div(R.div(camCoords.y, R.div(focalPlane.height,2)),2), 0.5);
 
 Patches.setScalarValue("mu", u);
 Patches.setScalarValue("mv", R.sub(1.0,v));
-
-/*
-
-
-
-face.nose.tip.monitor().subscribe(function(event) {
-
-
-
-    // Canvas is in Camera Space
- //   rect.transform.x = R.mul(R.div(camCoords.x,R.div(focalPlane.width,2)), R.div(canv.width,2));
-  //  rect.transform.y = R.mul(R.div(camCoords.y,R.div(focalPlane.height,2)), R.div(canv.height,2));
-});
-
-*/
