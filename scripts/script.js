2711f77f654dc623ec75911eda5eb74d073a87be
kavin kumar <bkavin001@gmail.com>
2021-05-02 19:04:32 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,16 +6,19 @@ const Shaders = require('Shaders');
 const R = require('Reactive');
 const Patchers = require('Patches');
 const face = FaceTracking.face(0);
-const rect = Scene.root.find("rectangle0");
-const cam = Scene.root.find("Camera");
-const canv = Scene.root.find("canvas0");
+(async () => {
+    const rect = await Scene.root.findFirst("rectangle0");
+    const cam = await Scene.root.findFirst("Camera");
+    const canv = await Scene.root.findFirst("canvas0");
 
-var focalPlane = cam.focalPlane;
-const camCoords = face.cameraTransform.applyTo(face.nose.tip);
+    var focalPlane = cam.focalPlane;
+    const camCoords = face.cameraTransform.applyTo(face.nose.tip);
 
 
-var u = R.add(R.div(R.div(camCoords.x, R.div(focalPlane.width,2)),2), 0.5);
-var v = R.add(R.div(R.div(camCoords.y, R.div(focalPlane.height,2)),2), 0.5);
+    var u = R.add(R.div(R.div(camCoords.x, R.div(focalPlane.width, 2)), 2), 0.5);
+    var v = R.add(R.div(R.div(camCoords.y, R.div(focalPlane.height, 2)), 2), 0.5);
 
-Patches.setScalarValue("mu", u);
-Patches.setScalarValue("mv", R.sub(1.0,v));
+    Patches.inputs.setScalar("mu", u);
+    Patches.inputs.setScalar("mv", R.sub(1.0, v));
+
+})()
